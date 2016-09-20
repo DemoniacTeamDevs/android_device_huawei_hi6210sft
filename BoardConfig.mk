ab40684c1530952d72b3cca9f42fc00b4c083748
hak86 <hakamelassouad@gmail.com>
2016-09-20 22:20:35 +0200

    hi6210sft: add initial Bluetooth

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,8 +17,10 @@ TARGET_2ND_CPU_ABI2 	:= armeabi
 TARGET_2ND_CPU_VARIANT 	:= cortex-a15
 
 # Bluetooth
+BOARD_BLUEDROID_VENDOR_CONF 			:= device/huawei/hi6210sft/bluetooth/vnd_hi6210sft.txt
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR 	:= "device/huawei/hi6210sft/bluetooth"
 BOARD_HAVE_BLUETOOTH 				:= true
+BOARD_HAVE_BLUETOOTH_BCM 			:= true
 
 # Board
 BOARD_HAS_LARGE_FILESYSTEM 	:= true
