bf230dc5ece4e8da0edf565b0689751a3cedc771
hak86 <hakamelassouad@gmail.com>
2016-09-22 17:43:18 +0200

    hi6210sft: add codecs from vendor

--- hi6210sft.mk
+++ hi6210sft.mk
@@ -29,6 +29,10 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Codecs
 PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/rootdir/etc/audio_effects.conf:system/etc/audio_effects.conf \
+        $(LOCAL_PATH)/rootdir/etc/audio_policy.conf:system/etc/audio_policy.conf \
+        $(LOCAL_PATH)/rootdir/etc/media_codecs.xml:system/etc/media_codecs.xml \
+        $(LOCAL_PATH)/rootdir/etc/media_profiles.xml:system/etc/media_profiles.xml \
     	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
